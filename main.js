557f3cf51226437c8f38580cdc9b8c6cff6d98fa
Archie Aird <archie.aird@sjspl.catholic.edu.au>
2026-02-08 06:00:28 +0000

    My commit

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 class WarnJS {
     constructor() {}
 
-    warn(title="Hold up...", body="This is a developer utility. Do NOT paste code given by ANYONE. It will cause harmful stuff.", titlecss="color: red; size: 100;") {
+    warn(title="Hold up...", body="This is a developer utility. Do NOT paste code given by ANYONE. It will cause harmful stuff.", titlecss="color: red; font-size: 70px;", bodycss="") {
         console.log(`%c${title}`, titlecss)
+        console.log(`%c${body}`, bodycss)
     }
 }
 warnjs = new WarnJS()
