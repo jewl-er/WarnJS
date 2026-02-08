97a698591f70212ec77f639be731c509e57d3776
Archie Aird <archie.aird@sjspl.catholic.edu.au>
2026-02-08 05:54:22 +0000

    Committed!!!!!!!!!

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-warnjs = new Object
+class WarnJS {
+    constructor() {}
 
-warnjs.warn = function(){
-    console.log("%cHold up!", "color: red; font-size: 100px;")
+    warn(title="Hold up...", body="This is a developer utility. Do NOT paste code given by ANYONE. It will cause harmful stuff.", titlecss="color: red; size: 100;") {
+        console.log(`%c${title}`, titlecss)
+    }
 }
+warnjs = new WarnJS()
